dbe277443e125744c940b22fa88800324904416f
Koichi Suematsu <pascal.jp@gmail.com>
2019-09-25 05:31:11 +0900

    Fix a problem in sample.js

--- sample.js
+++ sample.js
@@ -1,12 +1,8 @@
 const IBS_TH1 = require('./ibs_th1');
 
 const callback = data => {
-  if (data['error']) {
-    console.error(data['error']);
-    return;
-  }
-  console.log(data['address'], data['uuid'], data['date'], data['temperature'],
-	      data['humidity'], data['probeType'], data['battery']);
+  console.log(data.address, data.uuid, data.date, data.temperature,
+	      data.humidity, data.probeType, data.battery);
 };
 
 const device = new IBS_TH1();
