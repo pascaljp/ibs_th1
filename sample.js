5ab1e206e4eaa6ecc86942823899d83822aa3082
Koichi Suematsu <pascal.jp@gmail.com>
2019-09-25 16:26:03 +0900

    Stop accessing an obsolete data from sample program.

--- sample.js
+++ sample.js
@@ -1,8 +1,8 @@
 const IBS_TH1 = require('./ibs_th1');
 
 const callback = data => {
-  console.log(data.address, data.uuid, data.date, data.temperature,
-	      data.humidity, data.probeType, data.battery);
+  console.log(data.address, data.date, data.temperature, data.humidity,
+	      data.probeType, data.battery);
 };
 
 const device = new IBS_TH1();
